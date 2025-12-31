aa5f2b53b9f16f19943ad0f615b3002cf0e16400
4CkM0rth <sebastianrc0801@gmail.com>
2025-12-31 17:57:00 -0500

    fix: solution bug of class usercontroller

--- src/main/java/com/tolbargy/model/User.java
+++ src/main/java/com/tolbargy/model/User.java
@@ -17,7 +17,7 @@ import java.util.List;
 public class User implements UserDetails {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //El id se incrementa automaticamente al crear usuarios nuevos
     private Integer id;
 
     @Column(unique = true, nullable = false)
